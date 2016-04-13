2fe65a6af9aabeb7866830660e7e3d27e8110c17
Ethasham Nasir <alijange@gmail.com>
2016-04-13 06:33:00 -0400

    Applied fixes from StyleCI

--- main.php
+++ main.php
@@ -10,40 +10,44 @@ License: GPLv2 or later
 */
 
 // Runs when plugin is activated and creates new database field
-register_activation_hook(__FILE__,'like_dislike_counter_install');
+register_activation_hook(__FILE__, 'like_dislike_counter_install');
 add_action('admin_init', 'ldc_plugin_redirect');
-function ldc_plugin_activate() {
+function ldc_plugin_activate()
+{
     add_option('ldc_plugin_do_activation_redirect', true);
 }
 
-function ldc_plugin_redirect() {
+function ldc_plugin_redirect()
+{
     if (get_option('ldc_plugin_do_activation_redirect', false)) {
         delete_option('ldc_plugin_do_activation_redirect');
         wp_redirect('plugins.php');
     }
 }
-function like_dislike_counter_install() 
+function like_dislike_counter_install()
 {
-	ldc_plugin_activate();
+    ldc_plugin_activate();
 }
 
+function ldc_get_version()
+{
+    if (!function_exists('get_plugins')) {
+        require_once ABSPATH.'wp-admin/includes/plugin.php';
+    }
+    $plugin_folder = get_plugins('/'.plugin_basename(dirname(__FILE__)));
+    $plugin_file = basename((__FILE__));
 
-function ldc_get_version(){
-	if ( ! function_exists( 'get_plugins' ) )
-	require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-	$plugin_folder = get_plugins( '/' . plugin_basename( dirname( __FILE__ ) ) );
-	$plugin_file = basename( ( __FILE__ ) );
-	return $plugin_folder[$plugin_file]['Version'];
+    return $plugin_folder[$plugin_file]['Version'];
 }
 
-function ldc_lite_enqueue_css(){
-	if(!is_admin()){
-		wp_register_style($handle = 'like-dislike', plugins_url( 'like-dislike-counter-styles.css' , __FILE__ ), $deps = array(), $ver = '1.0.0', $media = 'all');
-		wp_enqueue_style('like-dislike');
-	}
+function ldc_lite_enqueue_css()
+{
+    if (!is_admin()) {
+        wp_register_style($handle = 'like-dislike', plugins_url('like-dislike-counter-styles.css', __FILE__), $deps = [], $ver = '1.0.0', $media = 'all');
+        wp_enqueue_style('like-dislike');
+    }
 }
 add_action('wp_print_styles', 'ldc_lite_enqueue_css');
 
-
-require_once('like.func.php');
-require_once('dislike.func.php');
+require_once 'like.func.php';
+require_once 'dislike.func.php';
